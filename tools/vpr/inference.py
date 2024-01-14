f73af984f2cd19a22757d8fc97e504fe8f980949
Jett Chen <jettchen12345@gmail.com>
2024-01-14 14:47:38 +0800

    Add mps inference acceleration(yay)

--- tools/vpr/inference.py
+++ tools/vpr/inference.py
@@ -4,6 +4,9 @@ from PIL import Image
 from typing import List
 import torchvision.transforms as transforms
 from pathlib import Path
+from langchain.tools import tool
+
+from coords import Coords
 
 model = None
 base_transform = transforms.Compose([
@@ -20,6 +23,9 @@ def get_model():
     if model is not None:
         return model
     model = torch.hub.load("gmberton/eigenplaces", "get_trained_model", backbone="ResNet50", fc_output_dim=2048)
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        mps_device = torch.device("mps")
+        model.to(mps_device)
     return model
 
 def weight_im(im: Image.Image | List[Image.Image]):
@@ -27,6 +33,9 @@ def weight_im(im: Image.Image | List[Image.Image]):
     if not isinstance(im, list):
         im = [im]
     input_tensor = torch.stack([base_transform(i) for i in im])
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        mps_device = torch.device("mps")
+        input_tensor = input_tensor.to(mps_device)
     output = mod(input_tensor)
     return output
 
@@ -44,10 +53,29 @@ def loc_sim(target: Image.Image, db: List[Image.Image]):
     s = cosine_similarity(w_target, w_db)
     return s
 
+@tool("Streetview Locate")
+def locate_image(im_loc: str, db_loc:str):
+    """
+    Locates an image using the streetview database. Must use the streetview tool to download the database first.
+    :param im_loc: the location of the image to be located
+    :param db_loc: the location of a coordinate csv file whose auxiliary information contains the location to the downloaded streetview images
+    :return:
+    """
+    TOP_N = 5
+    db_coords = Coords.from_csv(db_loc)
+    im = load_image(im_loc)
+    res = loc_sim(im, [load_image(x['image_path']) for x in db_coords.auxiliary])
+    top_n = torch.argsort(res, descending=True)[:TOP_N]
+    res_paths = "".join([f"{db_coords.auxiliary[i]['panorama_id']}: {db_coords.auxiliary[i]['image_path']}\n" for i in top_n])
+    return f"""
+    Top {TOP_N} results:
+    {res_paths}
+    """
+
 if __name__ == '__main__':
     res = loc_sim(
-        load_image('./ds/query.png'),
-        [load_image(f'../../run/streetview_res{i}.png') for i in range(49)]
+        load_image('./tools/vpr/ds/query.png'),
+        [load_image(f'./run/streetview_res{i}.png') for i in range(70)]
     )
-    print(res)
+    print(list(enumerate(res)))
     print(torch.argsort(res))
