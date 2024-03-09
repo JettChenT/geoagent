e3342ea5309ee049b6f60bfb08c77ff22af962fb
Jett Chen <jettchen12345@gmail.com>
2024-03-09 15:38:54 +0800

    Add tool for image search

--- src/tools/serp.py
+++ src/tools/serp.py
@@ -8,15 +8,33 @@ from functools import cache
 from PIL import Image
 from .. import utils
 from .wrapper import Session, gtool
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 SERP_API_KEY = os.environ["SERP_API_KEY"]
 TOP_N = 15
 
+def process_single_result(v, session):
+    try:
+        im_url = v["original"]
+        img_data = requests.get(im_url).content
+        im = Image.open(io.BytesIO(img_data))
+    except Exception as e:
+        im_url = v["thumbnail"]
+        img_data = requests.get(im_url).content
+        im = Image.open(io.BytesIO(img_data))
+    saved_loc = utils.save_img(im, "serp", session)
+    return f"Result: \n Title:{v['title']} \n {utils.image_to_prompt(saved_loc, session)}\n"
 
-@gtool("Google Lens Search", cached=True)
-def search_img(img_id: str, session: Session):
+def proc_image_results(results: dict, session: Session, top_n=TOP_N) -> str:
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_single_result, v, session) for i, v in enumerate(results[:top_n])]
+        res = [future.result() for future in as_completed(futures)]
+    return "".join(res)
+
+@gtool("google_lens", cached=True)
+def search_lens(img_id: str, session: Session):
     """
-    Searches Google Lens for an image
+    Searches Google Lens for an image (img -> content, relevant images)
     :param img_id: The image id
     :return:
     """
@@ -31,19 +49,32 @@ def search_img(img_id: str, session: Session):
     if "visual_matches" not in results:
         return f"Error: {results['error']}" if "error" in results else "No results found."
     visual_matches = results["visual_matches"]
-    res = ""
-    for i, v in enumerate(visual_matches[:TOP_N]):
-        im_url = v["thumbnail"]
-        # download the image
-        img_data = requests.get(im_url).content
-        im = Image.open(io.BytesIO(img_data))
-        saved_loc = utils.save_img(im, "serp", session)
-        res += (
-            f"Result {i}: \n Title:{v['title']} \n {utils.image_to_prompt(saved_loc, session)}\n"
-        )
-    return res
+    return proc_image_results(visual_matches, session)
+
+@gtool("google_image_search", cached=True)
+def image_search(query: str, session: Session):
+    """
+    Searches Google Images for a query (text query -> images, descriptions)
+    :param query: The query to search for
+    :return:
+    """
+    params = {
+        "api_key": SERP_API_KEY,
+        "engine": "google_images",
+        "q": query,
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    if "images_results" not in results:
+        return f"Error: {results['error']}" if "error" in results else "No results found."
+    images_results = results["images_results"]
+    return proc_image_results(images_results, session)
+
 
 
 if __name__ == "__main__":
+    from rich import print
     dotenv.load_dotenv()
-    print(search_img._run("./images/ukr_citycenter.jpg"))
+    session = Session()
+    image_search = image_search.to_tool(session)
+    print(image_search("The White House"))
